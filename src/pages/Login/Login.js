1a37316077e9d4ce1402a5e29edf1eec30296db3
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-03-30 17:40:13 +0900

    로그인 페이지 html구조 완성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import iconKakao from 'assets/Login/icon_kakao.png';
 import './Login.scss';
 
+const PW_REG_EXP =
+  /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,}$/;
+
 const Login = () => {
-  return <div className="header" />;
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState({
+    userId: '',
+    userPassword: '',
+  });
+
+  const getUserInfo = e => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleLogin = e => {
+    e.preventDefault();
+
+    fetch('http://10.58.52.65:3000/users/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: userInfo.userId,
+        password: userInfo.userPassword,
+      }),
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.accessToken) {
+          localStorage.setItem('token', data.accessToken);
+          window.location.replace('/');
+        } else {
+          alert('아이디와 비밀번호를 확인해주세요!');
+        }
+      });
+  };
+
+  const { userId, userPassword } = userInfo;
+
+  const isInputValid = userId.includes('@') && PW_REG_EXP.test(userPassword);
+
+  return (
+    <div className="login">
+      <h4 className="title-login">로그인</h4>
+      <div className="login-form-container">
+        <form className="login-form" onSubmit={handleLogin}>
+          <div className="form-wrap">
+            <input
+              onChange={getUserInfo}
+              id="input-email"
+              name="userId"
+              type="text"
+              placeholder="아이디"
+            />
+          </div>
+
+          <div className="form-wrap">
+            <input
+              onChange={getUserInfo}
+              id="input-password"
+              name="userPassword"
+              type="password"
+              placeholder="비밀번호"
+            />
+          </div>
+
+          <button className="login-button" disabled={!isInputValid}>
+            로그인
+          </button>
+        </form>
+
+        <div className="social-button">
+          <button className="loginButton-kakao" type="submit">
+            <img className="social-icon" src={iconKakao} />
+            카카오로 1초만에 시작하기
+          </button>
+        </div>
+
+        <div className="signupAndFind">
+          <Link to="/register" className="link">
+            회원가입
+          </Link>
+          <Link to="/login" className="link">
+            아이디 찾기
+          </Link>
+          <Link to="/login" className="link">
+            비밀번호 찾기
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;

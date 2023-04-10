1e456392cd4165c1ece739ea18668c106c87cd63
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-04-10 23:13:47 +0900

    404 페이지

--- src/Router.js
+++ src/Router.js
@@ -10,6 +10,7 @@ import Register from './pages/Register/Register';
 import Terms from './pages/Register/Component/Terms';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
+import PageNotFound from 'pages/404/PageNotFound';
 function Router() {
   return (
     <BrowserRouter>
@@ -24,7 +25,7 @@ function Router() {
         <Route path="/personalcolor" element={<PersonalColor />} />
         <Route path="/register" element={<Register />} />
         <Route path="/register/terms" element={<Terms />} />
-        <Route path="*" element={<div>없는 페이지입니다</div>} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>

06e41b5a8fe578391ad09cde1a8621b86f5e089c
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-04-26 16:06:39 +0900

    회원가입 페이지 완료

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,56 +1,154 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Register.scss';
 
 function Register() {
+  const [nickname, setNickName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+  const [pwSame, setPwSame] = useState('');
+  const [nickValid, setNickValid] = useState(false);
+  const [emailValid, setEmailValid] = useState(false);
+  const [pwValid, setPwValid] = useState(false);
+  const [pwSameValid, setPwSameValid] = useState(false);
+  const [allow, setAllow] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (nickValid && emailValid && pwValid && pwSameValid) {
+      setAllow(true);
+    } else {
+      setAllow(false);
+    }
+  }, [nickValid, emailValid, pwValid, pwSameValid]);
+
+  const handleNick = e => {
+    setNickName(e.target.value);
+    if (nickname.length >= 2) {
+      setNickValid(true);
+    } else {
+      setNickValid(false);
+    }
+  };
+
+  const handleEmail = e => {
+    setEmail(e.target.value);
+    const regex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (regex.test(e.target.value)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  };
+
+  const handlePw = e => {
+    setPw(e.target.value);
+    const regex =
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+    if (regex.test(e.target.value)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
+  };
+
+  const handlePwSame = e => {
+    setPwSame(e.target.value);
+    if (e.target.value === pw) {
+      setPwSameValid(true);
+    } else {
+      setPwSameValid(false);
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!allow) {
+      alert('회원가입에 실패하였습니다.');
+      return;
+    } else {
+      alert(`${nickname}님 회원가입이 완료되었습니다.`);
+      navigate('/login');
+    }
+  };
   return (
-    <div className="container">
-      <div className="contents">
-        <h4 className="signup"> 회원가입</h4>
-
-        <div className="signup-form">
-          <div className="form-wrap">
-            <label htmlFor="email">이메일</label>
-            <input type="text" placeholder="이메일을 입력해주세요" />
-            <p className="alert-txt">올바른 이메일을 입력해주세요</p>
+    <div className="register-container">
+      <h1 className="title">회원가입</h1>
+      <form className="register-form" method="POST">
+        <div className="register-input-wrap">
+          <div className="register-title"> 닉네임 </div>
+          <input
+            className="register-input"
+            type="text"
+            placeholder="닉네임을 입력해주세요"
+            onChange={handleNick}
+            value={nickname}
+          />
+          <div className="errorMessageWrap">
+            {!nickValid && nickname.length > 0 && (
+              <div>3글자 이상 입력해주세요.</div>
+            )}
+            {nickValid && nickname.length > 0 && (
+              <div>사용 가능한 닉네임입니다.</div>
+            )}
           </div>
-
-          <div className="form-wrap">
-            <label htmlFor="pw">비밀번호</label>
-            <input
-              name="userpassword"
-              type="password"
-              placeholder="영문, 숫자, 특수문자를 포함해서 8자 이상으로 입력해주세요"
-            />
-            <p className="alert-txt">비밀번호를 입력해주세요</p>
-          </div>
-
-          <div className="form-wrap">
-            <label htmlFor="pw2">비밀번호 확인</label>
-            <input
-              name="userpassword2"
-              type="password"
-              placeholder="비밀번호를 한번 더 입력해주세요"
-            />
-            <p className="alert-txt">비밀번호가 일치하지 않습니다.</p>
-          </div>
-          <div className="form-wrap">
-            <div className="nickname-repeat">
-              <label htmlFor="nickname">닉네임</label>
-              <input name="usernickname" type="text" placeholder="닉네임" />
-              <button className="check-repeat">중복확인</button>
-            </div>
-
-            <p className="alert-name">
-              닉네임을 설정하지 않을 시,
-              <br /> 성함이 가운데 부분이 * 처리된 상태로 노출됩니다. <br />
-              <span> 예시 홍*동 </span>
-            </p>
-
-            <button className="check-adult">휴대폰으로 성인인증하기</button>
-          </div>
-          <button className="submit-btn">가입 완료</button>
         </div>
-      </div>
+        <div className="register-input-wrap">
+          <div className="register-title"> 이메일을 입력하세요 </div>
+          <input
+            className="register-input"
+            type="text"
+            placeholder="숫자,영문,특수문자 포함된 이메일"
+            value={email}
+            onChange={handleEmail}
+          />
+          <div className="errorMessageWrap">
+            {!emailValid && email.length > 0 && (
+              <div>올바른 이메일을 입력해주세요.</div>
+            )}
+            {emailValid && email.length > 0 && (
+              <div>사용가능한 이메일입니다.</div>
+            )}
+          </div>
+        </div>
+
+        <div className="register-input-wrap">
+          <div className="register-title"> 비밀번호를 입력하세요 </div>
+          <input
+            className="register-input"
+            type="password"
+            placeholder="비밀번호"
+            value={pw}
+            onChange={handlePw}
+          />
+          <div className="errorMessageWrap">
+            {!pwValid && pw.length > 0 && (
+              <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+            )}
+            {pwValid && pw.length > 0 && <div>사용가능한 비밀번호입니다.</div>}
+          </div>
+        </div>
+        <div className="register-input-wrap">
+          <div className="register-title"> 비밀번호 재확인 </div>
+          <input
+            className="register-input"
+            type="password"
+            placeholder="비밀번호 재확인"
+            onChange={handlePwSame}
+            value={pwSame}
+          />
+          <div className="errorMessageWrap">
+            {!pwSameValid && pw.length > 0 && (
+              <div>비밀번호와 일치하지 않습니다.</div>
+            )}
+            {pwSameValid && pw.length > 0 && <div>비밀번호와 일치합니다.</div>}
+          </div>
+        </div>
+      </form>
+      <button className="register-button" onClick={handleSubmit}>
+        가입하기
+      </button>
     </div>
   );
 }

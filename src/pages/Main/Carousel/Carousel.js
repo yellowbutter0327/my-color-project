698ae3910c9399000acf153ab7fe1e97b89f4603
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-04-26 17:49:37 +0900

    FEAT: 메인페이지에서 이제품어때요탭 구현 완료

--- src/pages/Main/Carousel/Carousel.js
+++ src/pages/Main/Carousel/Carousel.js
@@ -24,7 +24,7 @@ const Carousel = () => {
   };
 
   return (
-    <Slider {...settings} className="slider">
+    <Slider {...settings} className="main-slider">
       <div>
         <img src={image1} className="slide-image" alt="slide 1" />
       </div>

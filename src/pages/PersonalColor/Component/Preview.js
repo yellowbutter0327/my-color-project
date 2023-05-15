43eec9d6172402fe40c655c12f7dd1b5df05bb6c
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-05-15 14:48:51 +0900

    MODIFY : 컬러팔레트 색상을 map함수로 간결하게 정리

--- src/pages/PersonalColor/Component/Preview.js
+++ src/pages/PersonalColor/Component/Preview.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Preview.scss';
-
 function Preview() {
   const [image, setImage] = useState('');
 
@@ -16,25 +15,110 @@ function Preview() {
   }
 
   return (
-    <div className="preview-container">
-      <label
-        htmlFor="type-file"
-        className={`file-wrap ${!image ? '' : 'no-image'}`}
-      >
-        <input
-          className="file-image"
-          type="file"
-          id="type-file"
-          accept="image/*"
-          onChange={ImageChangeEvent}
-        />
-        <div className="file-image-handler" />
-      </label>
-      {!image && <p className="preview-message">사진을 넣어주세요</p>}
-      {image && <img src={image} alt="Selected file" className="uploadImg" />}
-      <div class="bg" />
+    <div className="color-check-wrap">
+      <div className="color-left-wrap">
+        {seasonLeft.map(season => (
+          <div className="color-picker">
+            <h4>{season.name}</h4>
+            <div className="color-box">
+              {season.colors.map(color => (
+                <button className="color" style={{ background: color }} />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="preview-container">
+        <label
+          htmlFor="type-file"
+          className={`file-wrap ${!image ? '' : 'no-image'}`}
+        >
+          <input
+            className="file-image"
+            type="file"
+            id="type-file"
+            accept="image/*"
+            onChange={ImageChangeEvent}
+          />
+          <div className="file-image-handler" />
+        </label>
+        {!image && <p className="preview-message">사진을 넣어주세요</p>}
+        {image && <img src={image} alt="Selected file" className="uploadImg" />}
+        <div class="bg" />
+      </div>
+
+      <div className="color-right-wrap">
+        {seasonRight.map(season => (
+          <div className="color-picker">
+            <h4>{season.name}</h4>
+            <div className="color-box">
+              {season.colors.map(color => (
+                <button className="color" style={{ background: color }} />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Preview;
+
+const seasonLeft = [
+  {
+    name: 'Spring',
+    colors: [
+      '#f4cda4',
+      '#f2b3a8',
+      '#4c7f82',
+      '#94d3ca',
+      '#7b639f',
+      '#005072',
+      '#61aa51',
+      '#9ec6f0',
+    ],
+  },
+  {
+    name: 'Summer',
+    colors: [
+      '#eadbca',
+      '#43393a',
+      '#624f4c',
+      '#43393a',
+      '#e2a4b3',
+      '#7273ad',
+      '#487187',
+      '#436659',
+    ],
+  },
+];
+
+const seasonRight = [
+  {
+    name: 'Autumn',
+    colors: [
+      '#c97947',
+      '#caab55',
+      '#6d6e2b',
+      '#983636',
+      '#e9dbc3',
+      '#d7af84',
+      '#1f2950',
+      '#000000',
+    ],
+  },
+  {
+    name: 'Winter',
+    colors: [
+      '#7a2840',
+      '#215f4f',
+      '#164669',
+      '#443359',
+      '#2a2a37',
+      '#1a2b50',
+      '#c3c1c2',
+      '#fefefe',
+    ],
+  },
+];
